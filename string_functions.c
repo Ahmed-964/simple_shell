9e6ba896981ade5ce116e6cc555b199fbed893d8
Ahmed-964 <125926099+Ahmed-964@users.noreply.github.com>
2023-09-20 02:42:51 +0300

    Update string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -1,77 +1,75 @@
 #include "shell.h"
 
 /**
- * This function returns the length of a string.
- * 
- * @param str The string whose length to check
- * @return The length of the string
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
  */
-int get_string_length(char *str)
+int _strlen(char *s)
 {
-    int length = 0;
+	int i = 0;
 
-    if (!str)
-        return 0;
+	if (!s)
+		return (0);
 
-    while (*str++)
-        length++;
-    return length;
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- * This function performs lexicographic comparison of two strings.
- * 
- * @param str1 The first string
- * @param str2 The second string
- * @return Negative if str1 < str2, positive if str1 > str2, zero if str1 == str2
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-int compare_strings(char *str1, char *str2)
+int _strcmp(char *s1, char *s2)
 {
-    while (*str1 && *str2)
-    {
-        if (*str1 != *str2)
-            return (*str1 - *str2);
-        str1++;
-        str2++;
-    }
-    if (*str1 == *str2)
-        return 0;
-    else
-        return (*str1 < *str2 ? -1 : 1);
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
 }
 
 /**
- * This function checks if a string starts with a specific substring.
- * 
- * @param str The string to search
- * @param substr The substring to find
- * @return Address of the next character of str if it starts with substr, or NULL otherwise
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
  */
-char *check_start(const char *str, const char *substr)
+char *starts_with(const char *haystack, const char *needle)
 {
-    while (*substr)
-    {
-        if (*substr++ != *str++)
-            return NULL;
-    }
-    return (char *)str;
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
 }
 
 /**
- * This function concatenates two strings.
- * 
- * @param dest The destination buffer
- * @param src The source buffer
- * @return Pointer to the destination buffer
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
  */
-char *concatenate_strings(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-    char *result = dest;
+	char *ret = dest;
 
-    while (*dest)
-        dest++;
-    while (*src)
-        *dest++ = *src++;
-    *dest = *src;
-    return result;
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
 }
