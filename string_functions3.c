5a532b2f7b11add474688d81ed56c37a68b52468
Ahmed-964 <125926099+Ahmed-964@users.noreply.github.com>
2023-09-20 02:21:28 +0300

    Update string_functions3.c

--- string_functions3.c
+++ string_functions3.c
@@ -1,74 +1,75 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ * _strncpy_betty - Copies a string with a limit.
+ * @dest: The destination string to be copied to.
+ * @src: The source string.
+ * @n: The maximum number of characters to be copied.
+ *
+ * Return: The concatenated string.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy_betty(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+    int i = 0, j;
+    char *result = dest;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			dest[j] = '\0';
-			j++;
-		}
-	}
-	return (s);
+    while (src[i] != '\0' && i < n - 1)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    if (i < n)
+    {
+        j = i;
+        while (j < n)
+        {
+            dest[j] = '\0';
+            j++;
+        }
+    }
+    return (result);
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ * _strncat_betty - Concatenates two strings with a limit.
+ * @dest: The first string.
+ * @src: The second string.
+ * @n: The maximum number of bytes to be used.
+ *
+ * Return: The concatenated string.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat_betty(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+    int i = 0, j;
+    char *result = dest;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+    while (dest[i] != '\0')
+        i++;
+    j = 0;
+    while (src[j] != '\0' && j < n)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    if (j < n)
+        dest[i] = '\0';
+    return (result);
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ * _strchr_betty - Locates a character in a string.
+ * @s: The string to be parsed.
+ * @c: The character to look for.
+ *
+ * Return: A pointer to the memory area s.
  */
-char *_strchr(char *s, char c)
+char *_strchr_betty(char *s, char c)
 {
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
+    do {
+        if (*s == c)
+            return (s);
+    } while (*s++ != '\0');
 
-	return (NULL);
+    return (NULL);
 }
